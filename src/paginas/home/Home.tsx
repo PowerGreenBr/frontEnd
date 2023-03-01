710518939bd9a934f7d81233968254a22b47c430
Jessica Cavalcante <je.cavalcante@outlook.com>
2023-03-01 19:38:35 -0300

    Criação do Projeto React e Componentes

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Home.css';
+
+function Home (){
+    return(
+        <>
+            <h1> Iniciar Texto PowerGreenBr</h1>
+            <p>Dissertar texto sobre os PowerBank</p>
+        </>
+    );
+}export default Home;
