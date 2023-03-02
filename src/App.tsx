a6b0b8e83e9fca2f1c1725f6e79317164ce8ff96
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-03-02 19:27:12 -0300

    Criadas Navbar e Footer do Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
 
+
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+
   );
 }
 
