3919bd0048465e13fdf15a5ecaea0bf42047ac93
WagnerMor <wagnermor@outlook.com>
2023-03-03 00:53:15 -0300

    Correcao: By Carol e Wagner - Routes em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,24 @@ import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
+import Cadastro from './paginas/cadastro/Cadastro';
+import { Login } from '@mui/icons-material';
 
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
 
   );
 }
