a8d216f7def0b5e3fae1251bdd6d3e460d6fdd74
Jessica Cavalcante <je.cavalcante@outlook.com>
2023-03-13 14:07:03 -0300

    Novo:ListaProdutos e ModalProdutos

--- src/paginas/lista_produtos/ListaProdutos.tsx
+++ src/paginas/lista_produtos/ListaProdutos.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import {Box} from '@mui/material';
+import './ListaPostagem.css';
 
 function ListaProdutos() {
+
   return (
-    <div>ListaProdutos</div>
-  )
+    <>
+      <Box m={2} >
+        <Card variant="outlined">
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Nome Produto
+            </Typography>
+            <Typography variant="h5" component="h2">
+              descrição
+            </Typography>
+            <Typography variant="body2" component="p">
+              Foto
+            </Typography>
+            <Typography variant="body2" component="p">
+              Valor
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="center" mb={1.5}>
+
+              <Link to="" className="text-decorator-none" >
+                <Box mx={1}>
+                  <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                    atualizar
+                  </Button>
+                </Box>
+              </Link>
+              <Link to="" className="text-decorator-none">
+                <Box mx={1}>
+                  <Button variant="contained" size='small' color="secondary">
+                    deletar
+                  </Button>
+                </Box>
+              </Link>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+    </>)
 }
 
-export default ListaProdutos
+export default ListaProdutos;
---
+++ src/paginas/lista_produtos/modal_produtos/ModalProdutos.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import { Box } from '@mui/material';
+import CloseIcon from '@material-ui/icons/Close';
+import './ModalProduto.css';
+import DetalhesProduto from '../../detalhes_produto/DetalhesProduto';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }),
+);
+
+function ModalProduto() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <Box display="flex" justifyContent="flex-end" className="cursor">
+        <CloseIcon onClick={handleClose} />
+      </Box>
+      <DetalhesProduto />
+    </div>
+  );
+
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        className="btnModal"
+        onClick={handleOpen}
+      >
+        Novo Produto
+      </Button>
+    </div>
+  );
+}
+
+export default ModalProduto;
