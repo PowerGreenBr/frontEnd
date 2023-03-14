fce9b482d7f3cdac735124a7722d730d48b6ad85
Marlon Bassoto <corretor.bassoto@gmail.com>
2023-03-14 09:02:26 -0300

    renomeado arquivos seguindo sintaxe do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Home from './paginas/home/Home';
-import Cadastro from './paginas/cadastro/Cadastro';
+import Cadastro from './paginas/cadastroUsuario/CadastroUsuario';
 import Login from './paginas/login/Login';
 import Footer from './componentes/estaticos/footer/Footer';
 
--- src/componentes/categoria/deletarCategoria/DeletarCategoria.tsx
+++ src/componentes/categoria/deletarCategoria/DeletarCategoria.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Card, CardActions, CardContent, Button, Typography} from '@material-ui/core';
 import {Box} from '@mui/material';
-import './DeletarTema.css';
+import './DeletarCategoria.css';
 import {useNavigate, useParams } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
 import { buscaId, deleteId } from '../../../services/Service';
-import Tema from '../../../model/Tema';
+import Categoria from '../../../model/Categoria';
 
 
 function DeletarTema() {
