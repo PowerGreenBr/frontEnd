6da413644f5cebfa1f833850859e3c63ae5861d2
Jessica Cavalcante <je.cavalcante@outlook.com>
2023-03-27 18:17:48 -0300

    Alteração: Estilização e adição do icones de Carousel

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -3,15 +3,19 @@ import './Home.css';
 import { Carousel } from '@trendyol-js/react-carousel';
 import { Typography } from '@material-ui/core';
 import { Box } from "@mui/material";
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 export default function Home (){
+
+  
 	return(
 		<>
     <Box textAlign='center'>
     <Typography >
-			<h1 >Locação de Baterias Para Veículos Elétricos Por Assinatura </h1>
+			<h1 >Locação de baterias para veículos elétricos por assinatura </h1>
 			</Typography>
-      <p >Bem-vindo à Power Green BR,
+      <p>Bem-vindo à Power Green BR,
         a empresa líder em locação de baterias para veículos elétricos
         por assinatura. Estamos comprometidos em fornecer uma solução 
         sustentável e acessível para a mobilidade elétrica, permitindo
@@ -22,12 +26,12 @@ export default function Home (){
         da bateria em seu veículo. Junte-se a nós agora e torne-se parte
         da mudança para um futuro mais verde e limpo.</p>
       
-                <Carousel show={2} slide={1} >
-                <img src="https://i.imgur.com/V96FdnV.jpg" className='img' />
-                <img src="https://i.imgur.com/R4S3PGP.jpg" className='img' />
-                <img src="https://i.imgur.com/gUXbaZZ.jpg" className='img' />
-                <img src="https://i.imgur.com/dcUnvaU.jpg" className='img' />
-                <img src="https://i.imgur.com/vpCvw2t.jpg" className='img' />
+                <Carousel show={2} slide={1} rightArrow={<ChevronRightIcon fontSize="large"  />} leftArrow={<ChevronLeftIcon fontSize="large" />} className='carousel'>
+                  <img src="https://i.imgur.com/V96FdnV.jpg" className='img' />
+                  <img src="https://i.imgur.com/R4S3PGP.jpg" className='img' />
+                  <img src="https://i.imgur.com/gUXbaZZ.jpg" className='img' />
+                  <img src="https://i.imgur.com/dcUnvaU.jpg" className='img' />
+                  <img src="https://i.imgur.com/vpCvw2t.jpg" className='img' />
                 </Carousel>
               </Box>
 		</>
