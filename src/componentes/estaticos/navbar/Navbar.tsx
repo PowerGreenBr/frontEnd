3a2008a4672321189fa8b66d3ead0e5b1e8d2858
WagnerMor <wagnermor@outlook.com>
2023-03-28 01:51:40 -0300

    Alteracao: responsividade do menu

--- src/componentes/estaticos/navbar/Navbar.tsx
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import { Box } from '@mui/material';
-
 import { TokenState } from '../../../store/tokens/tokensReducer';
 import { addToken } from '../../../store/tokens/actions';
 import './Navbar.css'
+
+
 function Navbar() {
+	const [classOn, setClassOn] = useState<boolean>(true);
 	
 	let dispatch = useDispatch()
 	const token = useSelector<TokenState, TokenState['token']>(
@@ -30,75 +30,65 @@ function Navbar() {
 		});
 		navigate('/login');
 	}
-	let navbarComponent
 
-	if (token !== '/') {
-		
-		
-	}
 	return(
-		<>
-			{navbarComponent}
-			<Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-		<img src="https://imgur.com/gl3eMog.png" alt="Banner" style={{ width: "100%", display: "flex", alignItems: "center" }}/>
-	</Box>
-	
-			<AppBar  position="static">
-			<Toolbar variant="dense" style={{ backgroundColor: "#4ca004" , justifyContent: 'center'}}>
-					
-					<Box display="flex" >
-						<Link to="/home" className="text-decorator-none">
-							<Box mx={1} className='cursor'>
-								<Typography className='espaco navbarColor' variant="h6" color="inherit">
-									Home
-								</Typography>
-							</Box>
-						</Link>
-						<Link to="/produtos" className="text-decorator-none">
-							<Box mx={1} className='cursor'>
-								<Typography className='espaco navbarColor' variant="h6" color="inherit">
-									Produtos
-								</Typography>
-							</Box>
-						</Link>
-						<Link to="/planos" className="text-decorator-none">
-							<Box mx={1} className='cursor'>
-								<Typography className='espaco navbarColor' variant="h6" color="inherit">
-									Planos
-								</Typography>
-							</Box>
-						</Link>
-						<Link to="/assinar" className="text-decorator-none">
-							<Box mx={1} className='cursor'>
-								<Typography className='espaco navbarColor' variant="h6" color="inherit">
-									Assinatura
-								</Typography>
-							</Box>
-						</Link>
-						<Link to="/cadastrar-planos" className="text-decorator-none">
-							<Box mx={1} className='cursor'>
-								<Typography className='espaco navbarColor' variant="h6" color="inherit">
-									Cadastrar Plano
-								</Typography>
-							</Box>
-						</Link>
-						<Link to="/cadastrar-produto" className="text-decorator-none">
-							<Box mx={1} className='cursor'>
-								<Typography className='espaco navbarColor' variant="h6" color="inherit">
-									Cadastrar Produto
-								</Typography>
-							</Box>
-						</Link>
-						<Box mx={1} className='sair' onClick={goLogout}>
-							<Typography className='espaco navbarColor' variant="h6" color="inherit">
-								Sair
-							</Typography>
-						</Box>
-					</Box>
-				</Toolbar>
-			</AppBar>
-		
-		</>
+		<header>
+    	<div className="container">
+				<div className='logo'>
+					<div>
+						<p>PowerGreen</p>
+						<p>Brasil</p>
+					</div>
+      	</div>
+
+      	<div className = { classOn ? 'menu-section on' : 'menu-section'} onClick={() => setClassOn(!classOn)}>
+					<div className="menu-toggle">
+						<div className="one"></div>
+						<div className="two"></div>
+						<div className="three"></div>
+					</div>
+
+					<nav>
+						<ul>
+							<li>
+								<Link to='/home'>
+									HOME
+								</Link>
+							</li>
+							<li>
+								<Link to='/produtos'>
+                PRODUTOS
+								</Link>
+							</li>
+							<li>
+								<Link to='/planos'>
+	            		PLANOS
+								</Link>
+							</li>
+              <li>
+								<Link to='/assinar'>
+            			ASSINATURAS
+								</Link>
+              </li>
+              <li>
+								<Link to='/cadastrar-planos'>
+            			CADASTRAR PLANO
+								</Link>
+              </li>
+              <li>
+								<Link to='/cadastrar-produto'>
+            			CADASTRAR PRODUTO
+								</Link>
+              </li>
+              <li onClick={goLogout}>
+                <span className='sair'>SAIR</span>
+              </li>
+            </ul>
+          </nav>
+
+				</div>
+			</div>
+		</header>
 	)
 }
 export default Navbar;
