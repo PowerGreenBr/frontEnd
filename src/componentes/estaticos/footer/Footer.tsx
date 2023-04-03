f398ae404a4d4b786a46e337ebd00b2ce5df0633
WagnerMor <wagnermor@outlook.com>
2023-04-03 14:49:08 -0300

    Alteracao: estilizacao Footer.tsx

--- src/componentes/estaticos/footer/Footer.tsx
+++ src/componentes/estaticos/footer/Footer.tsx
@@ -4,16 +4,34 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
 	return (
+		<>
 		<footer className={styles.footer}>
-			<a href='https://github.com/PowerGreenBr' target={'_blank'} rel="noreferrer">
-        <GitHub />
-      </a>
-			<h3>
-				<a href="https://bit.ly/powergreenbr" target={'_blank'} rel="noreferrer">
-          Conheça nosso projeto e equipe de desenvolvimento
-        </a>
-			</h3>
-			<p>© 2023 Copyright - Colaboradores PowerGreen|Brasil</p>
+			<div className={styles.prefooter}>
+				<div>
+					<h3>Equipe</h3>
+					<ul>
+						<li>Carolina Bertulli</li>
+						<li>Henrique</li>
+						<li>Jessica Cavalcante</li>
+						<li>Joel Junior</li>
+						<li>Marlon Bassoto</li>
+						<li>Wagner Moreira</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className={styles.footerFooter}>
+				<a href='https://github.com/PowerGreenBr' target={'_blank'} rel="noreferrer">
+  	      <GitHub />
+    	  </a>
+				<p>
+					<a href="https://bit.ly/powergreenbr" target={'_blank'} rel="noreferrer">
+          	Conheça nosso projeto e equipe de desenvolvimento
+        	</a>
+				</p>
+				<p>© 2023 Copyright - Colaboradores PowerGreen|Brasil</p>
+			</div>
 		</footer>
+		</>
 	)
 }
